b10074a103d445fed07d2ad3be94b7b92f4f4b67
baranemreturkmen <baranemre1997@gmail.com>
2023-12-19 08:58:55 +0300

    review api added.

---
+++ movies-backend/src/main/java/com/javaet/movies/repository/ReviewRepository.java
@@ -0,0 +1,8 @@
+package com.javaet.movies.repository;
+
+import com.javaet.movies.entity.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
