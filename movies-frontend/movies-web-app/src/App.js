8b7c12d7fd02b4304249bc1050b7d45f449ea9ce
baranemreturkmen <baranemre1997@gmail.com>
2024-02-01 23:19:42 +0300

    Watching youtube trailer functionality added.

--- movies-frontend/movies-web-app/src/App.js
+++ movies-frontend/movies-web-app/src/App.js
@@ -5,8 +5,9 @@ import {useState, useEffect} from 'react';
 import Layout from './components/Layout';
 import {Routes, Route} from 'react-router-dom';
 import Home from "./components/home/Home"
-import './App.css'
+import './App.css';
 import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
 
 function App() {
 
@@ -35,6 +36,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout></Layout>}>
           <Route path="/" element={<Home movies={movies}></Home>}></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer></Trailer>}></Route>
         </Route>
       </Routes>
     </div>  
